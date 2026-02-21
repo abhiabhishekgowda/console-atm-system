9fd78b6024643ac7dcce9d3e34299f9d1b481999
Abhishek.k <ceoabhi.k@gmail.com>
2026-02-21 11:00:41 +0530

    Add files via upload
    Day 4: Implemented PIN retry system (3 attempts) and daily withdrawal limit with validation

--- python atm.py
+++ python atm.py
@@ -9,68 +9,76 @@ accounts = {
 # CHECK IF THE NAME EXISTS IN OUR DATABASE
 name = input("Enter your name: ").lower().strip()
 if name in accounts:
-    enter_pin = input("Enter your pin: ")
-    if enter_pin == accounts[name]["pin"]:
-        print(f"Welcome, {name}!")
-        is_running = True
-        while is_running:
-            print(f"\n---{name}'s SESSION---")
-            print("1. Balance | 2. Deposit | 3. Withdraw | 4. Check History | 5. Exit")
-            try:
-                choice = int(input("Enter your choice: "))
-            except ValueError:
-                print("Please enter a number!")
-                continue
-            
-            if choice == 1:
-                print(f"Your balance is: {accounts[name]['balance']}")
-            
-            elif choice == 2:
+    attempts = 3
+    while attempts > 0:
+        enter_pin = input("Enter your PIN: ")
+        if enter_pin == accounts[name]["pin"]:
+            print(f"Welcome, {name}!")
+
+            is_running = True
+            daily_limit = 0
+            while is_running:
+
+                print(f"\n---{name}'s SESSION---")
+                print("1. Balance | 2. Deposit | 3. Withdraw | 4. Check History | 5. Exit")
                 try:
-                    deposit = int(input("Enter deposit amount: "))
-                    if deposit > 0:
-                        accounts[name]['balance'] += deposit
-                        accounts[name]['history'].append(f"Deposited {deposit}")
-                        print("Deposit successful!")
-                    else:
-                        print("Invalid amount!")
+                    choice = int(input("Enter your choice: "))
                 except ValueError:
-                    print("Invalid input. Please enter a number.")
+                    print("Please enter a number!")
+                    continue
             
-            elif choice == 3:
-                try:
-                    withdraw = int(input("Enter withdrawal amount: "))
-                    if 0 < withdraw <= accounts[name]['balance']:
-                        accounts[name]['balance'] -= withdraw
-                        accounts[name]['history'].append(f"Withdrew {withdraw}")
-                        print("Withdrawal successful!")
-                    else:
-                        print("Insufficient funds or invalid amount!")
-                except ValueError:
-                    print("Invalid input. Please enter a number.")
+                if choice == 1:
+                    print(f"Your balance is: {accounts[name]['balance']}")
             
-            elif choice == 4:
-                print("Your transaction history:")
-                for h in accounts[name]['history']:
-                    print("-", h)
+                elif choice == 2:
+                    try:
+                        deposit = int(input("Enter deposit amount: "))
+                        if deposit > 0:
+                            accounts[name]['balance'] += deposit
+                            accounts[name]['history'].append(f"Deposited {deposit}")
+                            print("Deposit successful!")
+                        else:
+                            print("Invalid amount!")
+                    except ValueError:
+                        print("Invalid input. Please enter a number.")
             
-            elif choice == 5:
-                print("Exiting... Thank you!")
-                is_running = False
+                elif choice == 3:
+                    try:
+                        withdraw = int(input("Enter withdrawal amount: "))
+                        if withdraw <= 0:
+                            print("Invalid amount!")
+                        elif withdraw > accounts[name]['balance']:
+                            print("Insufficient funds!")
+                        elif daily_limit + withdraw > 15000:
+                            print("Daily withdrawal limit exceeded!(15000)")
+                        else:
+                            accounts[name]['balance'] -= withdraw
+                            accounts[name]['history'].append(f"Withdrew {withdraw}")
+                            daily_limit += withdraw
+                            print("Withdrawal successful!")
+                    except ValueError:
+                        print("Invalid input. Please enter a number.")
             
-            else:
-                print("Invalid choice, try again.")
-    else:
-        print("Incorrect Pin code.")
+                elif choice == 4:
+                    print("Your transaction history:")
+                    for h in accounts[name]['history']:
+                        print("-", h)
+            
+                elif choice == 5:
+                    print("Exiting... Thank you!")
+                    is_running = False
+            
+                else:
+                    print("Invalid choice, try again.")
+            break
+        else:
+            attempts -= 1
+            print(f"Wrong PIN. Attempts left: {attempts}")
+    if attempts == 0:
+        print("Account blocked due to too many failed attempts.")
 else:
     print("Name not found in database.")
                     
-
-
-            
-                
-
-
 
 
 
